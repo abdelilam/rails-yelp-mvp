5b3e8808e4310e1e601ef47f2025c8244cb4b000
Abdelilah El hotmani <abdelilah.elhotmani@outlook.com>
2018-11-21 19:35:10 +0100

    Restaurants and reviwes impemeted with success

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+
 Rails.application.routes.draw do
+  get root to: 'restaurants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, except: [:destroy, :update, :edit] do
+    resources :reviews, only: [:create, :new]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants_attributes = [
+  {
+    name:         'Venice',
+    address:      '7 Boundary St, Roma E2 7JE',
+    phone_number: '077888999',
+    category:      'italian'
+  },
+  {
+    name:         'passe compose',
+    address:      '56A Shoreditch High St, Montreal E1 6PQ',
+    phone_number: '74040904',
+    category:      'french'
+  },
+
+  {
+    name:         'The wok',
+    address:      '56A young street, Rabat E1 6PQ',
+    phone_number: '242425252',
+    category:      'chinese'
+  },
+
+  {
+    name:         'Chocolatte',
+    address:      '5 rue la revolution, paris E1 6PQ',
+    phone_number: '0020442245',
+    category:      'belgian'
+  },
+
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '55566677',
+    category:      'italian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
